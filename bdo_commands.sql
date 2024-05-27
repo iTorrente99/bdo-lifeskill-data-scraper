6380326f1e1ebc85856b1e2148677529569cda4f
iTorrente99 <itorrente99@gmail.com>
2024-05-27 02:46:38 +0200

    proxies and better db

--- bdo_commands.sql
+++ bdo_commands.sql
@@ -1,24 +1,22 @@
---SELECT * FROM adventurer_data WHERE guild IN ('Clarity', 'Life') ORDER BY cooking_total_level DESC;
-
--- SELECT
---     guild,
---     ROUND(AVG(gathering_total_level), 2) AS avg_gathering,
---     ROUND(AVG(fishing_total_level), 2) AS avg_fishing,
---     ROUND(AVG(hunting_total_level), 2) AS avg_hunting,
---     ROUND(AVG(cooking_total_level), 2) AS avg_cooking,
---     ROUND(AVG(alchemy_total_level), 2) AS avg_alchemy,
---     ROUND(AVG(processing_total_level), 2) AS avg_processing,
---     ROUND(AVG(training_total_level), 2) AS avg_training,
---     ROUND(AVG(trading_total_level), 2) AS avg_trading,
---     ROUND(AVG(farming_total_level), 2) AS avg_farming,
---     ROUND(AVG(sailing_total_level), 2) AS avg_sailing,
---     ROUND(AVG(barter_total_level), 2) AS avg_barter,
---     ROUND(AVG(total_level), 2) AS avg_total_level,
---     ROUND(AVG(life_fame), 2) AS avg_life_fame
--- FROM adventurer_data
--- WHERE date = '2024-05-26' AND total_level IS NOT NULL
--- GROUP BY guild
--- ORDER BY avg_total_level DESC;
+SELECT
+    guild,
+    ROUND(AVG(gathering_total_level), 2) AS avg_gathering,
+    ROUND(AVG(fishing_total_level), 2) AS avg_fishing,
+    ROUND(AVG(hunting_total_level), 2) AS avg_hunting,
+    ROUND(AVG(cooking_total_level), 2) AS avg_cooking,
+    ROUND(AVG(alchemy_total_level), 2) AS avg_alchemy,
+    ROUND(AVG(processing_total_level), 2) AS avg_processing,
+    ROUND(AVG(training_total_level), 2) AS avg_training,
+    ROUND(AVG(trading_total_level), 2) AS avg_trading,
+    ROUND(AVG(farming_total_level), 2) AS avg_farming,
+    ROUND(AVG(sailing_total_level), 2) AS avg_sailing,
+    ROUND(AVG(barter_total_level), 2) AS avg_barter,
+    ROUND(AVG(total_level), 2) AS avg_total_level,
+    ROUND(AVG(life_fame), 2) AS avg_life_fame
+FROM adventurer_data
+WHERE date = '2024-05-26' AND total_level IS NOT NULL
+GROUP BY guild
+ORDER BY avg_total_level DESC;
 
 SELECT
     date,
@@ -56,9 +54,11 @@ ORDER BY date DESC;
 
 
 SELECT 
+	ad1.date as start_d,
+	ad2.date as end_d,
     ad1.family_name,
     ad1.guild,
-	ad2.total_level,
+    ad2.total_level,
     (ad2.gathering_total_level - ad1.gathering_total_level) AS gathering_diff,
     (ad2.fishing_total_level - ad1.fishing_total_level) AS fishing_diff,
     (ad2.hunting_total_level - ad1.hunting_total_level) AS hunting_diff,
@@ -77,7 +77,44 @@ FROM
 JOIN 
     adventurer_data ad2 ON ad1.family_name = ad2.family_name AND ad1.guild = ad2.guild
 WHERE 
-    ad1.date = '2024-05-25' AND ad2.date = '2024-05-26' AND ad2.guild = 'Clarity'
-ORDER BY total_level_diff DESC;
+    ad1.date = '2024-05-25' AND ad2.date = '2024-05-26'
+    AND (ad2.guild = 'Clarity' OR ad2.guild = 'Life')
+    AND (ad2.total_level - ad1.total_level) > 0
+ORDER BY 
+    total_level_diff DESC;
+	
+	
+SELECT 
+	ad1.date as start_d,
+	ad2.date as end_d,
+    ad1.guild,
+    ROUND(AVG(ad2.gathering_total_level - ad1.gathering_total_level), 2) AS gathering_diff,
+    ROUND(AVG(ad2.fishing_total_level - ad1.fishing_total_level), 2) AS fishing_diff,
+    ROUND(AVG(ad2.hunting_total_level - ad1.hunting_total_level), 2) AS hunting_diff,
+    ROUND(AVG(ad2.cooking_total_level - ad1.cooking_total_level), 2) AS cooking_diff,
+    ROUND(AVG(ad2.alchemy_total_level - ad1.alchemy_total_level), 2) AS alchemy_diff,
+    ROUND(AVG(ad2.processing_total_level - ad1.processing_total_level), 2) AS processing_diff,
+    ROUND(AVG(ad2.training_total_level - ad1.training_total_level), 2) AS training_diff,
+    ROUND(AVG(ad2.trading_total_level - ad1.trading_total_level), 2) AS trading_diff,
+    ROUND(AVG(ad2.farming_total_level - ad1.farming_total_level), 2) AS farming_diff,
+    ROUND(AVG(ad2.sailing_total_level - ad1.sailing_total_level), 2) AS sailing_diff,
+    ROUND(AVG(ad2.barter_total_level - ad1.barter_total_level), 2) AS barter_diff,
+    ROUND(AVG(ad2.total_level - ad1.total_level), 2) AS total_level_diff,
+    ROUND(AVG(ad2.life_fame - ad1.life_fame), 2) AS life_fame_diff,
+    ROUND(AVG(ad2.total_level), 2) AS avg_total_level,
+    ROUND(AVG(ad2.life_fame), 2) AS avg_life_fame
+FROM 
+    adventurer_data ad1
+JOIN 
+    adventurer_data ad2 ON ad1.family_name = ad2.family_name AND ad1.guild = ad2.guild
+WHERE 
+    ad1.date = '2024-05-25' AND ad2.date = '2024-05-26'
+GROUP BY 
+    ad1.guild
+ORDER BY 
+    avg_total_level DESC;
+
+
+
 
 
