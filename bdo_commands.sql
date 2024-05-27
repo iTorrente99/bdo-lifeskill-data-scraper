bf1f2dee5c700870e7c9f25c1a4eac12731fc0ec
iTorrente99 <itorrente99@gmail.com>
2024-05-27 02:09:37 +0200

    proxies and better db

--- bdo_commands.sql
+++ bdo_commands.sql
@@ -1,19 +1,83 @@
 --SELECT * FROM adventurer_data WHERE guild IN ('Clarity', 'Life') ORDER BY cooking_total_level DESC;
 
-SELECT 
-    guild,
-    ROUND(AVG(gathering_total_level), 2) AS avg_gathering_level,
-    ROUND(AVG(fishing_total_level), 2) AS avg_fishing_level,
-    ROUND(AVG(hunting_total_level), 2) AS avg_hunting_level,
-    ROUND(AVG(cooking_total_level), 2) AS avg_cooking_level,
-    ROUND(AVG(alchemy_total_level), 2) AS avg_alchemy_level,
-    ROUND(AVG(processing_total_level), 2) AS avg_processing_level,
-    ROUND(AVG(training_total_level), 2) AS avg_training_level,
-    ROUND(AVG(trading_total_level), 2) AS avg_trading_level,
-    ROUND(AVG(farming_total_level), 2) AS avg_farming_level,
-    ROUND(AVG(sailing_total_level), 2) AS avg_sailing_level,
-    ROUND(AVG(barter_total_level), 2) AS avg_barter_level
+-- SELECT
+--     guild,
+--     ROUND(AVG(gathering_total_level), 2) AS avg_gathering,
+--     ROUND(AVG(fishing_total_level), 2) AS avg_fishing,
+--     ROUND(AVG(hunting_total_level), 2) AS avg_hunting,
+--     ROUND(AVG(cooking_total_level), 2) AS avg_cooking,
+--     ROUND(AVG(alchemy_total_level), 2) AS avg_alchemy,
+--     ROUND(AVG(processing_total_level), 2) AS avg_processing,
+--     ROUND(AVG(training_total_level), 2) AS avg_training,
+--     ROUND(AVG(trading_total_level), 2) AS avg_trading,
+--     ROUND(AVG(farming_total_level), 2) AS avg_farming,
+--     ROUND(AVG(sailing_total_level), 2) AS avg_sailing,
+--     ROUND(AVG(barter_total_level), 2) AS avg_barter,
+--     ROUND(AVG(total_level), 2) AS avg_total_level,
+--     ROUND(AVG(life_fame), 2) AS avg_life_fame
+-- FROM adventurer_data
+-- WHERE date = '2024-05-26' AND total_level IS NOT NULL
+-- GROUP BY guild
+-- ORDER BY avg_total_level DESC;
+
+SELECT
+    date,
+    ROUND(AVG(gathering_total_level), 2) AS avg_gathering,
+    ROUND(AVG(fishing_total_level), 2) AS avg_fishing,
+    ROUND(AVG(hunting_total_level), 2) AS avg_hunting,
+    ROUND(AVG(cooking_total_level), 2) AS avg_cooking,
+    ROUND(AVG(alchemy_total_level), 2) AS avg_alchemy,
+    ROUND(AVG(processing_total_level), 2) AS avg_processing,
+    ROUND(AVG(training_total_level), 2) AS avg_training,
+    ROUND(AVG(trading_total_level), 2) AS avg_trading,
+    ROUND(AVG(farming_total_level), 2) AS avg_farming,
+    ROUND(AVG(sailing_total_level), 2) AS avg_sailing,
+    ROUND(AVG(barter_total_level), 2) AS avg_barter,
+    ROUND(AVG(total_level), 2) AS avg_total_level,
+    ROUND(AVG(life_fame), 2) AS avg_life_fame
 FROM adventurer_data
-GROUP BY guild;
+WHERE guild = 'Clarity' AND total_level IS NOT NULL
+GROUP BY date
+ORDER BY date DESC;
+
+
+
+
+
+-- UPDATE adventurer_data
+-- SET
+--     total_level = CASE WHEN total_level = 0 THEN NULL ELSE total_level END,
+--     life_fame = CASE WHEN life_fame = 0 THEN NULL ELSE life_fame END;
+
+-- UPDATE adventurer_data
+-- SET life_fame = life_fame + 1
+-- WHERE date = '2024-05-26';
+
+
+
+SELECT 
+    ad1.family_name,
+    ad1.guild,
+	ad2.total_level,
+    (ad2.gathering_total_level - ad1.gathering_total_level) AS gathering_diff,
+    (ad2.fishing_total_level - ad1.fishing_total_level) AS fishing_diff,
+    (ad2.hunting_total_level - ad1.hunting_total_level) AS hunting_diff,
+    (ad2.cooking_total_level - ad1.cooking_total_level) AS cooking_diff,
+    (ad2.alchemy_total_level - ad1.alchemy_total_level) AS alchemy_diff,
+    (ad2.processing_total_level - ad1.processing_total_level) AS processing_diff,
+    (ad2.training_total_level - ad1.training_total_level) AS training_diff,
+    (ad2.trading_total_level - ad1.trading_total_level) AS trading_diff,
+    (ad2.farming_total_level - ad1.farming_total_level) AS farming_diff,
+    (ad2.sailing_total_level - ad1.sailing_total_level) AS sailing_diff,
+    (ad2.barter_total_level - ad1.barter_total_level) AS barter_diff,
+    (ad2.total_level - ad1.total_level) AS total_level_diff,
+    (ad2.life_fame - ad1.life_fame) AS life_fame_diff
+FROM 
+    adventurer_data ad1
+JOIN 
+    adventurer_data ad2 ON ad1.family_name = ad2.family_name AND ad1.guild = ad2.guild
+WHERE 
+    ad1.date = '2024-05-25' AND ad2.date = '2024-05-26' AND ad2.guild = 'Clarity'
+ORDER BY total_level_diff DESC;
 
 
--- scraper.py
+++ scraper.py
@@ -5,27 +5,38 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from itertools import cycle
 from bs4 import BeautifulSoup
 from utils import extract_profile_info
+from temp_db_queries import get_active_proxies
 
 class Scraper:
     def __init__(self):
+        # Lista de proxies basada en los puertos visibles en la imagen
+        self.proxies = get_active_proxies()
+        self.proxy_pool = cycle(self.proxies)
         self.driver = self.set_driver()
+
+        
 
     def set_driver(self):
         """Inicializa el navegador y lo minimiza."""
-        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        options = webdriver.ChromeOptions()
+        proxy = next(self.proxy_pool)
+        options.add_argument(f'--proxy-server={proxy}')
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
         driver.minimize_window()
         return driver
 
-    def get_guild_raw(self, guildName, region="EU", retries=5):
+    def get_guild_raw(self, guildName, region="EU", retries=50):
         """Obtiene la lista de miembros de una guild y sus perfiles con reintentos en caso de error."""
         url = f"https://www.naeu.playblackdesert.com/en-US/Adventure/Guild/GuildProfile?guildName={guildName}&region={region}"
         attempt = 0
+        print(f"Getting {guildName} guild data...")
         while attempt < retries:
             try:
                 self.driver.get(url)
-                WebDriverWait(self.driver, 1000).until(EC.presence_of_element_located((By.CLASS_NAME, "box_list_area")))
+                WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "box_list_area")))
                 # Obtener el HTML
                 box_list_area = self.driver.find_element(By.CLASS_NAME, "box_list_area")
                 html_data = box_list_area.get_attribute('innerHTML')
@@ -44,14 +55,17 @@ class Scraper:
                 members_data = self.get_members_data(members)
                 return members_data
             except Exception as e:
-                print(f"No se encontró el elemento para la URL: {url}. Intento {attempt + 1} de {retries}. Error: {e}")
+                print(f"No se encontró el elemento para la URL: {url}. Intento {attempt + 1} de {retries}.")
                 attempt += 1
-                #time.sleep(2)  # Esperar 2 segundos antes de reintentar
-                if attempt == retries:
+                if attempt < retries:
+                    # Cambiar de proxy y reiniciar el driver
+                    self.driver.quit()
+                    self.driver = self.set_driver()
+                else:
                     print(f"Se agotaron los intentos para la URL: {url}.")
                     return {}
 
-    def get_members_data(self, members, retries=5):
+    def get_members_data(self, members, retries=50):
         """Obtiene los datos de cada miembro de la guild con reintentos en caso de error."""
         members_data = {}
         for family_name, profile_url in members.items():
@@ -59,17 +73,20 @@ class Scraper:
             while attempt < retries:
                 try:
                     self.driver.get(profile_url)
-                    WebDriverWait(self.driver, 1000).until(EC.presence_of_element_located((By.CLASS_NAME, "box_profile_area")))
+                    WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "box_profile_area")))
                     box_profile_area = self.driver.find_element(By.CLASS_NAME, "box_profile_area")
                     html_data = box_profile_area.get_attribute('innerHTML')
                     members_data[family_name] = extract_profile_info(html_data)
                     members_data[family_name]['profile_url'] = profile_url
                     break  # Salir del bucle si la carga es exitosa
                 except Exception as e:
-                    print(f"No se encontró el elemento para la URL: {profile_url}. Intento {attempt + 1} de {retries}. Error: {e}")
+                    print(f"No se encontró el elemento para la URL: {profile_url}. Intento {attempt + 1} de {retries}.")
                     attempt += 1
-                    #time.sleep(2)  # Esperar 2 segundos antes de reintentar
-                    if attempt == retries:
+                    if attempt < retries:
+                        # Cambiar de proxy y reiniciar el driver
+                        self.driver.quit()
+                        self.driver = self.set_driver()
+                    else:
                         print(f"Se agotaron los intentos para la URL: {profile_url}.")
         
         return members_data
